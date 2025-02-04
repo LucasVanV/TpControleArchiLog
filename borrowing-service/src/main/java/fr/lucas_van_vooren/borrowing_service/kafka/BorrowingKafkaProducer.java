b82d4722cce5d153fe8bcbfb6c9b7bd0c488db5e
LucasVanV <lucas.vanvooren@outlook.com>
2025-02-04 16:36:38 +0100

    debug

--- borrowing-service/src/main/java/fr/lucas_van_vooren/borrowing_service/kafka/BorrowingKafkaProducer.java
+++ borrowing-service/src/main/java/fr/lucas_van_vooren/borrowing_service/kafka/BorrowingKafkaProducer.java
@@ -2,7 +2,9 @@ package fr.lucas_van_vooren.borrowing_service.kafka;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BorrowingKafkaProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
